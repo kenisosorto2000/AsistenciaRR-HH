31b378d038fee82ba744a0553dd3380a07c08ef6
Kenis Noe Osorto Reyes <kenisreyes11@gmail.com>
2025-06-05 13:12:00 -0600

    HEAD

--- marcaje/views.py
+++ marcaje/views.py
@@ -674,19 +674,12 @@ Este es un mensaje automático.
     # Si entran por GET
     return redirect('ausencias_encargado')
 
-<<<<<<< HEAD
-def error_404(request, exception):
-    return render(request, '404.html', status=404)
-
-def error_500(request):
-    return render(request, '500.html', status=500)
-=======
 # def error_404(request, exception):
 #     return render(request, '404.html', status=404)
 
 # def error_500(request):
 #     return render(request, '500.html', status=500)
->>>>>>> origin/devHector
+
 def fecha_corte(request):
     if request.method == 'POST':
         anio = request.POST.get('anio')
