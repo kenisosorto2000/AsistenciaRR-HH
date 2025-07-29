8efc6d9483435120a341c1da6ead38107f2612ce
Kenis Noe Osorto Reyes <kenisreyes11@gmail.com>
2025-07-29 11:49:15 -0600

    Añade función de solicitud de pago para RRHH
    Implementa la funcionalidad de solicitud de pago, lo que permite a RRHH gestionar las solicitudes de pago de vacaciones.
    
    Añade nuevos formularios y plantillas para enviar solicitudes de pago y cargar documentos de respaldo. Integra la autenticación de usuarios para restringir el acceso según el grupo de usuarios.
    
    Proporciona vistas para que RRHH revise, apruebe/rechace y haga seguimiento de las solicitudes de pago, incluyendo un historial. Integra HTMX para actualizaciones dinámicas y Alpine.js para interacciones modales.

--- turnos/urls.py
+++ turnos/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('ficha-vacaciones/<int:id>/', views.ficha_vacaciones_view, name='ficha_vacaciones'),
     path('crear-pago/', views.crear_pago, name='crear_pago'),
     path('subir-comprobante-pago/', views.subir_comprobante, name='subir_comprobante_pago'),
+    path('form-comprobante_pago/<int:pago_id>/', views.formulario_comprobantes, name="form_comprobante_pago"),
+    path('solicitudes-rh-pago/', views.solicitudes_rh_pago, name='solicitudes_rh_pago'),
+    path('accion-solicitud-pago/', views.accion_solicitud_p, name='accion_solicitud_pago'),
+    path('historial-solicitudes-pago/', views.ver_historial_solicitudes_pago, name='historial_solicitudes_pago'),
+    path('eliminar-pago-h/<int:pago_id>/', views.eliminar_pago_H, name='eliminar_pago_h'),
 ]
--- turnos/views.py
+++ turnos/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from marcaje.views import grupo_requerido
 from django.shortcuts import get_object_or_404
+from .forms_p import *
+from django.template.loader import render_to_string
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -20,11 +23,13 @@ def turnos_view(request):
 def ficha_view(request):
     return render(request, 'ficha_turnos.html')
 
+@login_required
 def ficha_vacaciones_view(request, id):
     pago = get_object_or_404(Pago, id=id)
     return render(request, 'ficha_vacaciones.html', {'solicitudes': pago})
 
-
+@login_required
+@grupo_requerido('encargado')
 def crear_pago(request):
     tipo_pagos = TipoPago.objects.all()
     encargados = Empleado.objects.filter(es_encargado=True)
@@ -96,7 +101,7 @@ def formulario_comprobantes(request, permiso_id):
 
 
 @login_required
-# @grupo_requerido('encargado')
+@grupo_requerido('encargado')
 def subir_comprobante(request):
     try:
         encargado = Empleado.objects.get(user=request.user)
@@ -120,3 +125,105 @@ def subir_comprobante(request):
     return render(request, 'subir_comprobante_pago.html', {
         'solicitudes': solicitudes,
     })
+@login_required
+@grupo_requerido('encargado')
+def formulario_comprobantes(request, pago_id):
+    pago = get_object_or_404(Pago, id=pago_id)
+    comprobante_qs = PagoComprobante.objects.filter(pago=pago)
+    if request.method == 'POST':
+        form = SubirComprobanteForm(request.POST, request.FILES, instance=comprobante_qs.first() if comprobante_qs.exists() else None)
+
+        if form.is_valid():
+            comprobante = form.save(commit=False)
+            comprobante.pago = pago
+            comprobante.save()
+            pago.tiene_comprobante = True
+            pago.pendiente_subsanar = False
+            pago.save()
+            html = render_to_string('act_fila_comp.html', {"pago": pago})
+            return HttpResponse(html)
+    else:
+        form = SubirComprobanteForm(instance=comprobante_qs.first() if comprobante_qs.exists() else None)
+
+    return render(request, "formulario_p.html", {"form": form, "pago": pago})
+
+@login_required
+@grupo_requerido('rrhh')
+def solicitudes_rh_pago(request):
+    estado = 'P'
+    pagos = Pago.objects.filter(Q(estado_solicitud=estado) | Q(estado_solicitud='SB')).order_by('-fecha_solicitud')
+
+    context = []
+    for pago in pagos:
+        comprobante = PagoComprobante.objects.filter(pago=pago).first()
+        context.append({
+            'pago': pago,
+            'estado_rh_display': pago.get_estado_solicitud_display(),  # ✅ más limpio
+            'comprobante': comprobante.comprobante.url if comprobante else None,
+        })
+
+    return render(request, 'solicitudes_rh_pago.html', {'pagos': context})
+
+@login_required
+@grupo_requerido('rrhh')
+def accion_solicitud_p(request):
+    if request.method == 'POST':
+        solicitud_id = request.POST.get('solicitud_id')
+        accion = request.POST.get('accion_realizada')  # 'aprobar' o 'rechazar'
+        revisado_por = request.POST.get('revisada_por')
+        comentario = request.POST.get('comentarios', '')
+        aprobacion_gerencial = request.POST.get('aprobacion_gerencial')
+        # 1. Busca la solicitud
+        solicitud = get_object_or_404(Pago, id=solicitud_id)
+        # 2. Define el nuevo estado
+        if accion == 'A':
+            nuevo_estado = 'A'
+            if aprobacion_gerencial:
+                solicitud.aprobacion_gerencial = True
+        elif accion == 'R':
+            nuevo_estado = 'R'
+        elif accion == 'SB':
+            nuevo_estado = 'SB'
+            solicitud.pendiente_subsanar = True
+        else:
+            return HttpResponse("Acción no válida", status=400)
+        # 3. Guarda el detalle
+        GestionPagoDetalle.objects.create(
+            solicitud=solicitud,
+            accion_realizada=nuevo_estado,
+            revisada_por=revisado_por,
+            comentarios=comentario
+        )
+        # 4. Actualiza el estado de la solicitud
+        solicitud.estado_solicitud = nuevo_estado
+        solicitud.save()
+        # 5. (Opcional) Retorna un mensaje, puedes actualizar la tabla con HTMX
+        return redirect('solicitudes_rh_pago')
+    return HttpResponse("Método no permitido", status=405)
+
+@login_required
+@grupo_requerido('rrhh')
+def ver_historial_solicitudes_pago(request):
+    historial = GestionPagoDetalle.objects.filter(solicitud__estado_solicitud__in=['A', 'R', 'SB']).order_by('-fecha')
+    context = []
+    for h in historial:
+        comprobante_obj = PagoComprobante.objects.filter(pago=h.solicitud).first()
+        comprobante_url = comprobante_obj.comprobante.url if comprobante_obj else None
+
+        context.append({
+            'detalle': h,
+            'estado_rh_display': h.solicitud.get_estado_solicitud_display(),
+            'comprobante_url': comprobante_url,
+        })
+    return render(request, 'historial_solicitudes_pago.html', {'solicitudes': context})
+
+@login_required
+@grupo_requerido('rrhh')
+def eliminar_pago_H(request, pago_id):
+    pago = get_object_or_404(Pago, id=pago_id)
+
+    if request.method == 'POST':
+        pago.delete()
+        return redirect('historial_solicitudes_pago')  # o donde quieras redirigir
+
+    return render(request, 'eliminar_pago_h.html', {'pago': pago})
