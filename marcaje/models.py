ae6a6da164dad44d4735cc21fed7e1a76b2590f3
Kenis Noe Osorto Reyes <kenisreyes11@gmail.com>
2025-07-30 16:08:17 -0600

    Agrega el logotipo de la empresa al modelo de la empresa.
    Permite asociar un logotipo a una empresa.
    
    Este cambio introduce un campo de logotipo en el modelo Empresa, lo que permite a los usuarios cargar y mostrar logotipos de empresas. Sustituye la selección de logotipos predefinida en la plantilla por una visualización dinámica del logotipo basada en el campo de logotipo de la empresa.

--- marcaje/models.py
+++ marcaje/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Empresa(models.Model):
     nombre = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='logos/', null=True, blank=True)
     def __str__(self):
         return self.nombre
 class Sucursal(models.Model):
